fbe887ce0637cb39afd006fbc736b098bdd1c99a
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-06-30 10:21:17 -0300

    feat: Add auth model

---
+++ src/app/core/models/auth.model.ts
@@ -0,0 +1,21 @@
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  refreshToken: string;
+  mfaRequired: boolean;
+  mfaToken: string | null;
+  status: string;
+}
+
+export interface MfaVerifyRequest {
+  token: string;
+  code: string;
+}
+
+export interface RefreshRequest {
+  refreshToken: string | null;
+}
