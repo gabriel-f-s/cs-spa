6c945c651f445aa1f09db9209a7823ea9ab4fb4c
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-06-30 10:17:44 -0300

    feat: Add Auth service

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import {
+  AuthResponse,
+  LoginRequest,
+  MfaVerifyRequest,
+  RefreshRequest,
+} from '../../models/auth.model';
+import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly API_URL: string = `${environment.apiUrl}/auth`;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  public login(credentials: LoginRequest, rememberMe: boolean): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.API_URL}/login`, credentials).pipe(
+      tap((response) => {
+        if (!response.mfaRequired && response.status === 'SUCCESS') {
+          this.setSession(response, rememberMe);
+        }
+      }),
+    );
+  }
+
+  public logout(): void {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    sessionStorage.clear();
+    this.router.navigate(['/auth/login']);
+  }
+
+  public refreshToken(): Observable<AuthResponse> {
+    const refreshToken: RefreshRequest = {
+      refreshToken: localStorage.getItem('refresh_token') || sessionStorage.getItem('refresh_token'),
+    }
+    return this.http.post<AuthResponse>(`${this.API_URL}/refresh`, { refreshToken }).pipe(
+      tap((response) => {
+        const isLocal = !!localStorage.getItem('refresh_token');
+        this.setSession(response, isLocal);
+      }),
+    );
+  }
+
+  public getToken(): string | null {
+    return localStorage.getItem('access_token') || sessionStorage.getItem('access_token');
+  }
+
+  public isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  public getUserRole(): string | null {
+    const token = this.getToken();
+    if (!token) return null;
+    try {
+      const decoded: any = jwtDecode(token);
+      return decoded.role || decoded.authorities;
+    } catch {
+      return null;
+    }
+  }
+
+  public verifyMfaAndLogin(request: MfaVerifyRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.API_URL}/mfa/verify`, request)
+  }
+
+  public setSession(authResult: AuthResponse, rememberMe: boolean): void {
+    const storage = rememberMe ? localStorage : sessionStorage;
+    (rememberMe ? sessionStorage : localStorage).clear();
+    storage.setItem('access_token', authResult.accessToken);
+    storage.setItem('refresh_token', authResult.refreshToken);
+  }
+}
