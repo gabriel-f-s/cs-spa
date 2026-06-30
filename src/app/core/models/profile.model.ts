e78ab263d1ef6dd4a812143127be2bd7efde3e94
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-06-30 10:21:05 -0300

    feat: Add profile model

---
+++ src/app/core/models/profile.model.ts
@@ -0,0 +1,52 @@
+export interface AdminUserSummaryResponse {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface ProfileResponse {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  role: string;
+  mfaEnabled: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface SecurityProfile {
+  mfaEnabled: boolean;
+  lastLoginAt?: Date;
+  lastLoginIP?: string;
+  lastLoginUserAgent?: string;
+}
+
+export interface MfaSetupResponse {
+  token: string;
+  otpAuthUri: string;
+}
+
+export interface MfaConfirmRequest {
+  code: string;
+}
+
+export interface UpdateProfileRequest {
+  name?: string;
+  phoneNumber?: string;
+}
+
+export interface ChangeEmailRequest {
+  email: string;
+  confirmEmail: string;
+}
+
+export interface ChangePasswordRequest {
+  password: string;
+  confirmPassword: string;
+}
+
+export interface DisableMfaRequest {
+  code: string;
+}
+
