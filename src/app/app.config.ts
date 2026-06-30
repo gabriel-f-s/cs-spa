de77ed6bb35b2005a9e3a4ed932c3929d9ed5a08
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-06-30 10:23:47 -0300

    feat: Integrate Taiga UI, global styles, and HTTP client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
+import { provideTaiga } from '@taiga-ui/core';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideTaiga(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+  ],
 };
